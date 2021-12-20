7946b1bc66d2acc514d121ca6f088ddac970032c
Benjamin Chabbert <b.chabbert@outlook.fr>
2021-12-20 18:18:27 +0100

    Initial commit

---
+++ src/main/java/fr/miage/bank/APIBanque.java
@@ -0,0 +1,13 @@
+package fr.miage.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIBanque {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIBanque.class, args);
+	}
+
+}
---
+++ src/main/java/fr/miage/bank/entity/Account.java
@@ -0,0 +1,3 @@
+package fr.miage.bank.entity;
+public class Account {
+}
