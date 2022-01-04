b28bd7a2bd6b3c4df860ca64da3422972317e60c
Benjamin Chabbert <b.chabbert@outlook.fr>
2022-01-04 23:31:43 +0100

    Gestion du User

--- src/main/java/fr/miage/bank/entity/UserInput.java
+++ src/main/java/fr/miage/bank/entity/UserInput.java
@@ -1,7 +1,10 @@
 package fr.miage.bank.entity;
 
 import lombok.*;
+
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Data
 @Getter
@@ -10,14 +13,30 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 public class UserInput {
 
+    @NotNull
     @Size(min = 3)
     private String lastname;
 
+    @NotNull
     @Size(min = 2)
     private String firstname;
 
+    @NotNull
     private String email;
 
+    @NotNull
     @Size(min = 4)
     private String password;
+
+    @NotNull
+    private Date birthdate;
+
+    @NotNull
+    private String country;
+
+    @NotNull
+    private String noPasseport;
+
+    @NotNull
+    private String noTel;
 }
