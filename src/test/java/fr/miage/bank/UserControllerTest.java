760093603b4fd9a60d4a81ecde7a19c2809f9c4b
Benjamin Chabbert <b.chabbert@outlook.fr>
2022-01-14 00:06:18 +0100

    Test PATCH et PUT sur User

--- src/test/java/fr/miage/bank/UserControllerTest.java
+++ src/test/java/fr/miage/bank/UserControllerTest.java
@@ -1,8 +1,12 @@
 package fr.miage.bank;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.miage.bank.entity.User;
+import fr.miage.bank.input.UserInput;
 import fr.miage.bank.repository.UserRepository;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +17,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 
 import java.util.Date;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -33,6 +38,7 @@ class UserControllerTest {
 
 	@Test
 	public void getAllUsersTest(){
+
 		User user1 = new User("1", "Parker", "Peter", new Date(), "France", "12534534","0606060606","peter@gmail.fr","1234");
 		userRepository.save(user1);
 		Response response = when().get("users")
@@ -59,4 +65,52 @@ class UserControllerTest {
 
 	}
 
+	@Test
+	public void saveUserTest() throws JsonProcessingException {
+		UserInput user = new UserInput("Parker", "Peter", "peter@gmail.fr", "123456",  new Date(), "France", "123465789","0606060606");
+		ObjectMapper map = new ObjectMapper();
+		Response response = given()
+				.body(map.writeValueAsString(user))
+				.contentType(ContentType.JSON)
+				.when()
+				.post("/users")
+				.then()
+				.statusCode(HttpStatus.SC_CREATED)
+				.extract()
+				.response();
+
+		Response response1 = when().get("users")
+				.then()
+				.statusCode(HttpStatus.SC_OK)
+				.extract()
+				.response();
+		String jsonAsString = response1.asString();
+		assertThat(jsonAsString, containsString(user.getFirstname()));
+	}
+
+	@Test
+	public void updateUserTest() throws JsonProcessingException {
+		User user = new User("1", "Parker", "Peter", new Date(), "France", "12534534","0606060606","peter@gmail.fr","1234");
+		userRepository.save(user);
+
+		UserInput user1 = new UserInput("Parker", "Peter", "peter@gmail.fr", "123456",  new Date(), "Angleterre", "123465789","0606060606");
+		ObjectMapper map = new ObjectMapper();
+		Response response = given()
+				.body(map.writeValueAsString(user1))
+				.contentType(ContentType.JSON)
+				.when()
+				.put("users/"+user.getId())
+				.then()
+				.statusCode(HttpStatus.SC_OK)
+				.extract()
+				.response();
+
+		Response response1 = when().get("users/"+user.getId())
+				.then()
+				.statusCode(HttpStatus.SC_OK)
+				.extract()
+				.response();
+		String jsonAsString = response1.asString();
+		assertThat(jsonAsString, containsString("Angleterre"));
+	}
 }
